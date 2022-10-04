fe6e58c8c66f9c409b9451bdaf30006dd22429a4
Ali <iamaliahad@outlook.com>
2022-10-04 18:16:09 +0800

    add design

---
+++ src/sections/Navbar.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <>
+    <div className="navbar">
+        <li className="navlist"> Home </li>
+        <li className="navlist"> Shop </li>
+        <li className="navlist"> Contact </li>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
